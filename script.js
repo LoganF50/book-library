68bb7e3d84080758e5e40162f3a5c4754dc238a3
Logan <faschingbauerl@yahoo.com>
2020-05-10 06:59:50 -0500

    update render function

--- script.js
+++ script.js
@@ -13,25 +13,69 @@ Book.prototype.info = function() {
   return `${this.title} by ${this.author}, ${this.numPages} pages, ` + hasReadStr;
 }
 
+Book.prototype.getDomNode = function(index) {
+  let domCard = document.createElement('div');
+  let domDelete = document.createElement('i');
+  let domTitle = document.createElement('div');
+  let domBy = document.createElement('div');
+  let domAuthor = document.createElement('div');
+  let domNumPages = document.createElement('div');
+  let domHasRead = document.createElement('div');
+  let domBtn = document.createElement('button');
+
+  domCard.classList.add('card');
+  domCard.dataset.index = `${index}`;
+
+  domDelete.classList.add('material-icons', 'card__btn_delete');
+  domDelete.innerText = 'delete';
+  domCard.appendChild(domDelete);
+
+  domTitle.classList.add('card__text', 'card__text--emphasized');
+  domTitle.innerText = this.title;
+  domCard.appendChild(domTitle);
+
+  domBy.classList.add('card__text');
+  domBy.innerText = 'by';
+  domCard.appendChild(domBy);
+
+  domAuthor.classList.add('card__text', 'card__text--emphasized');
+  domAuthor.innerText = this.author;
+  domCard.appendChild(domAuthor);
+
+  domNumPages.classList.add('card__text');
+  domNumPages.innerText = `${this.numPages} pages`;
+  domCard.appendChild(domNumPages);
+
+  domHasRead.classList.add('card__text');
+  domHasRead.innerText = this.hasRead ? 'read' : 'not read';
+  domCard.appendChild(domHasRead);
+
+  domBtn.classList.add('btn', 'btn--small');
+  domBtn.innerText = 'Change read status';
+  domCard.appendChild(domBtn);
+
+  return domCard;
+}
+
 function addBookToLibrary(book) {
   jsLibrary.push(book);
 }
 
+//TODO deletion of books (eventListener, function, etc.)
+//TODO toggling of hasRead (eventListener, function, etc.)
+//TODO different style when read to stand out (green text/checkmark)
+//TODO store data (localStorage vs Firebase)
+
 //TODO connect library to html
 function renderBooks() {
-  jsLibrary.forEach(book => {
-    let bookNode = document.createElement('div');
-    
-    <div class="card">
-        <i class="material-icons card__btn_delete">delete</i>
-        <div class="card__text card__text--emphasized">Harry Potter</div>
-        <div class="card__text">by</div>
-        <div class="card__text card__text--emphasized">JK Rowling</div>
-        <div class="card__text">435 pages</div>
-        <div class="card__text">already read</div>
-        <button class="btn btn--small">Change read status</button>
-      </div>
-  });
+  //remove current books
+  while(library.firstChild) {
+    library.removeChild(library.lastChild);
+  }
+  //add updated library
+  for(let i = 0; i < jsLibrary.length; i++) {
+    library.appendChild(jsLibrary[i].getDomNode(i));
+  }
 }
 
 //close modal form
@@ -77,6 +121,7 @@ function handleAddingBook(e) {
     const newBook = new Book(bookTitle.value, bookAuthor.value, bookNumPages.value, bookHasRead.checked);
     addBookToLibrary(newBook);
     //TODO render books
+    renderBooks();
     closeForm();
   }
 } 
@@ -109,8 +154,3 @@ window.onclick = function(e){
 btnOpenForm.addEventListener('click', openForm);
 btnCloseForm.addEventListener('click', closeForm);
 btnAddBook.addEventListener('click', handleAddingBook);
-
-
-//TODO add display for each book to delete from library
-//TODO add button to toggle haveRead value
-//TODO store data (localStorage vs Firebase)
